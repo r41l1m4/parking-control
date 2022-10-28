ea403c41f2560a625983881d34768e69c0e0b5be
r41l1m4 <rai.lima17@gmail.com>
2022-10-28 12:12:42 -0300

    create parking spot repository

---
+++ src/main/java/com/ironia/parkingcontrol/api/repository/ParkingSpotRepository.java
@@ -0,0 +1,11 @@
+package com.ironia.parkingcontrol.api.repository;
+
+import com.ironia.parkingcontrol.api.model.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+}
