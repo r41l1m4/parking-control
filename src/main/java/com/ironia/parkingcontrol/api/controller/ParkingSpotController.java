01a305c6ef69aebe26e4e7d2587e9541dd4347dc
r41l1m4 <rai.lima17@gmail.com>
2022-10-28 13:30:45 -0300

    create post resource and service method

--- src/main/java/com/ironia/parkingcontrol/api/controller/ParkingSpotController.java
+++ src/main/java/com/ironia/parkingcontrol/api/controller/ParkingSpotController.java
@@ -1,9 +1,16 @@
 package com.ironia.parkingcontrol.api.controller;
 
+import com.ironia.parkingcontrol.api.model.ParkingSpotDTO;
+import com.ironia.parkingcontrol.domain.model.ParkingSpotModel;
 import com.ironia.parkingcontrol.domain.service.ParkingSpotService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -16,5 +23,14 @@ public class ParkingSpotController {
         this.parkingSpotService = parkingSpotService;
     }
 
+    @PostMapping
+     public ResponseEntity<?> saveParkingSpot(@RequestBody @Valid ParkingSpotDTO parkingSpotDTO) {
+        var parkingSpotModel = new ParkingSpotModel();
+         BeanUtils.copyProperties(parkingSpotDTO, parkingSpotModel);
+         parkingSpotModel.setRegistrationDate(LocalDateTime.now((ZoneId.of("UTC"))));
 
+         return ResponseEntity
+                 .status(HttpStatus.CREATED)
+                 .body(parkingSpotService.save(parkingSpotModel));
+     }
 }
