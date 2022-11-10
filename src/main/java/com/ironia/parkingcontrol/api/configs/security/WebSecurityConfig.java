e54826db4581621a44f6292601f7484f016596fc
r41l1m4 <rai.lima17@gmail.com>
2022-11-10 15:01:41 -0300

    add in-memory auth

--- src/main/java/com/ironia/parkingcontrol/api/configs/security/WebSecurityConfig.java
+++ src/main/java/com/ironia/parkingcontrol/api/configs/security/WebSecurityConfig.java
@@ -1,11 +1,20 @@
 package com.ironia.parkingcontrol.api.configs.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,6 +23,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeHttpRequests()
                 .anyRequest()
 //                .permitAll();
-                .authenticated();
+                .authenticated()
+                .and()
+                .csrf().disable(); //desativar proteção para mitigar csrf (Cross-Site Reference Forgery)
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("root")
+                .password(passwordEncoder().encode("root"))
+                .roles("ADMIN");
     }
 }
